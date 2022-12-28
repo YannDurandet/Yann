00bae03f8ac9b77ae72e187704985fe47f7a082f
Yann Durandet <durandet.yann@gmail.com>
2022-12-29 00:46:32 +0100

    Like Holy fucking shit

--- index.js
+++ index.js
@@ -1,3 +1,4 @@
+/* DARK MODE TOGGLE */
 let contrastToggle = false;
 function toggleContrast() {
   contrastToggle = !contrastToggle;
@@ -7,3 +8,14 @@ function toggleContrast() {
     document.body.classList.remove("dark-theme")
   }
 }
+
+/* DARK MODE SWITCH THAT I FUCKING DESPISE */
+var btn = document.getElementById('btn')
+
+function leftClick() {
+	btn.style.left = '0'
+}
+
+function rightClick() {
+	btn.style.left = '36px';
+} 
---
+++ shitToFixAndStuffToAdd.md
@@ -0,0 +1,8 @@
+- Link hover effect in dark mode still black (should become white with the text)
+- almost not responsive at all
+- need more animations
+- dark mode switch doesnt properly work (you can toggle both ways by clicking only on 1 symbol, and it brakes the slider)
+
+- Cool looking transition between landing paeg and about me
+- moving background gradient
+- French English toggle
